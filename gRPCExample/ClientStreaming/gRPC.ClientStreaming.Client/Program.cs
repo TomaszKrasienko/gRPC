380e6baf980a4195274bb02dc42ccebdc6238f0a
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2023-10-21 13:22:10 +0200

    Added client implementation to connect with server

--- gRPCExample/ClientStreaming/gRPC.ClientStreaming.Client/Program.cs
+++ gRPCExample/ClientStreaming/gRPC.ClientStreaming.Client/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using gRPC.ClientStreaming.Server;
+using Grpc.Net.Client;
+
 Console.WriteLine("Hello, World!");
+
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new ExamResultsService.ExamResultsServiceClient(channel);
+
+using var call = client.SendResults();
+for (int i = 0; i < 10; i++)
+{
+    var request = new ExamTaskAnswerRequest()
+    {
+        TaskNumber = i + 1,
+        Result = false
+    };
+    await call.RequestStream.WriteAsync(request);
+}
+await call.RequestStream.CompleteAsync();
+var response = await call.ResponseAsync;
+Console.WriteLine(response);
