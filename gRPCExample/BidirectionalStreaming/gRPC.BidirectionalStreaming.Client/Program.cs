84f71c3b58d653af5e8c023d27f10eebf3e600ec
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2023-10-22 13:07:33 +0200

    Added client of bidirectional streaming

---
+++ gRPCExample/BidirectionalStreaming/gRPC.BidirectionalStreaming.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Net.Client;
+using gRPC.BidirectionalStreaming.Server;
+
+Console.WriteLine("Please type \"start\" to begin stream");
+string input = Console.ReadLine();
+
+
+
+if (input is "start")
+{
+    using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+    var client = new PositionService.PositionServiceClient(channel);
+    using var call = client.ShowResults();
+    string answer = string.Empty;
+
+    while (answer != "q")
+    {
+        Console.WriteLine("Read direction in a schema \"direction, distance\" ");
+        answer = Console.ReadLine();
+        string direction = answer.Split(",")[0];
+        string distance = answer.Split(",")[1];
+        await call.RequestStream.WriteAsync(new PositionRequest()
+        {
+            Direction = direction,
+            Distance = int.Parse(distance)
+        });
+        if (await call.ResponseStream.MoveNext(CancellationToken.None))
+        {
+            Console.WriteLine(call.ResponseStream.Current);
+        }
+
+    }
+}
